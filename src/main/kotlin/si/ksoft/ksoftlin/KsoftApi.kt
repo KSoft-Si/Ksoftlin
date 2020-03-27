6a2b4d9fdd7650a1a421a347f89740fcd0fe4ed8
chachy <shalomadecoolboy@outlook.com>
2020-03-27 19:35:03 +0000

    rewrite time

--- src/main/kotlin/si/ksoft/ksoftlin/KsoftApi.kt
+++
@@ -1,9 +0,0 @@
-package si.ksoft.ksoftlin
-
-import si.ksoft.ksoftlin.endpoints.ImageEndpoint
-import si.ksoft.ksoftlin.endpoints.LyricsEndpoint
-
-class KsoftApi(token: String) {
-    val imageEndpoint = ImageEndpoint(token)
-    val lyricsEndpoint = LyricsEndpoint(token)
-}
