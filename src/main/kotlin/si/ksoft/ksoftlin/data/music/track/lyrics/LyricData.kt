7796cb1c6c164e55a42211aa7f0ca951e8413356
ChachyDev <shalomadecoolboy@outlook.com>
2020-09-06 17:52:14 +0100

    Reformat code & Add URL field support :)

--- src/main/kotlin/si/ksoft/ksoftlin/data/music/track/lyrics/LyricData.kt
+++ src/main/kotlin/si/ksoft/ksoftlin/data/music/track/lyrics/LyricData.kt
@@ -4,21 +4,24 @@ import com.google.gson.annotations.SerializedName
 
 data class SongResponse(val data: List<Song>)
 
-data class Song(val artist: String,
-                val artistId: Int,
-                val album: String,
-                @SerializedName("album_ids")
-                val albumIds: String,
-                @SerializedName("album_year")
-                val albumYear: String,
-                val name: String,
-                val lyrics: String,
-                @SerializedName("search_str")
-                val searchString: String,
-                @SerializedName("album_art")
-                val albumArt: String,
-                val popularity: Int,
-                val id: String,
-                @SerializedName("search_score")
-                val searchScore: Double)
+data class Song(
+    val artist: String,
+    val artistId: Int,
+    val album: String,
+    @SerializedName("album_ids")
+    val albumIds: String,
+    @SerializedName("album_year")
+    val albumYear: String,
+    val name: String,
+    val lyrics: String,
+    @SerializedName("search_str")
+    val searchString: String,
+    @SerializedName("album_art")
+    val albumArt: String,
+    val popularity: Int,
+    val id: String,
+    @SerializedName("search_score")
+    val searchScore: Double,
+    val url: String
+)
 
